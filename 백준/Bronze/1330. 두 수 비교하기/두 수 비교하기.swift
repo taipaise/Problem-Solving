60919c34d295f99549be29f3974c66d6964e8721
taipaise <83569908+taipaise@users.noreply.github.com>
2023-03-25 18:18:51 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 12 ms, Memory: 79512 KB -BaekjoonHub

--- 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.swift
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.swift
@@ -4,20 +4,17 @@
 //
 //  Created by 이동현 on 2023/03/25.
 //
-
 import Foundation
 
 var nums = readLine()!.split(separator: " ").map{Int($0)!}
 var a = nums[0]
 var b = nums[1]
 
-if a > b{
+switch a{
+case Int.min..<b:
+    print("<")
+case b:
+    print("==")
+default:
     print(">")
 }
-else if a == b{
-    print("==")
-}
-else{
-    print("<")
-}
-
