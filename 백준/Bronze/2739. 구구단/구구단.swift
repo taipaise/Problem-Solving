fde36badcd012a974291d1f7550019019ea9d80e
taipaise <83569908+taipaise@users.noreply.github.com>
2023-03-25 14:00:00 +0900

    [Bronze V] Title: 구구단, Time: 16 ms, Memory: 79508 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  BOJ_2739
+//
+//  Created by 이동현 on 2023/03/25.
+//
+
+import Foundation
+
+var num = Int(readLine()!)!
+
+for i in 1...9{
+    print("\(num) * \(i) = \(num * i)")
+}
