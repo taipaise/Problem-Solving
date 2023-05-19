3149705f96105ec860510058745e11216045a6b7
taipaise <83569908+taipaise@users.noreply.github.com>
2023-05-19 18:57:40 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+#include <utility>
+#include <vector>
+
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL);cout.tie(NULL)
+#define endl "\n"
+#define rep(i, a, b) for(auto i = a; i < b; ++i)
+#define REP(i, a, b) for(auto i = a; i <= b; ++i)
+#define pii pair<int, int>
+#define all(v) (v).begin(), (v).end() 
+#define pb push_back
+#define INF numeric_limits<int>::max()
+#define PIV 1 << 20
+
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+
+int main(void){
+    
+    cout << "       _.-;;-._\n'-..-'|   ||   |\n'-..-'|_.-;;-._|\n'-..-'|   ||   |\n'-..-'|_.-''-._|";
+}
