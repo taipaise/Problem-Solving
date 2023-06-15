bebecb075941f3d92f9dd939f1fd386284795f23
taipaise <83569908+taipaise@users.noreply.github.com>
2023-06-15 13:14:44 +0900

    [Bronze V] Title: 등록, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL);cout.tie(NULL)
+#define endl "\n"
+#define rep(i, a, b) for(auto i = a; i < b; ++i)
+#define REP(i, a, b) for(auto i = a; i <= b; ++i)
+#define pii pair<int, int>
+#define all(v) (v).begin(), (v).end() 
+#define pb push_back
+#define INF numeric_limits<int>::max()
+#define PIV 1 << 20
+
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+
+int main(void){
+    cout << 215<<"\n" <<"taipaise\n";
+}
