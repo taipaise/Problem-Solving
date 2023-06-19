1f28e078a60194ff9869aef130b7e9f43da2c50f
taipaise <83569908+taipaise@users.noreply.github.com>
2023-06-19 23:21:59 +0900

    [Bronze V] Title: 스타워즈 로고, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9653. 스타워즈 로고/스타워즈 로고.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define rep(i, a, b) for(auto i = a; i < b; ++i)
+#define REP(i, a, b) for(auto i = a; i <= b; ++i)
+#define endl "\n"
+
+using namespace std;
+
+int main(void){
+cout << "    8888888888  888    88888\n   88     88   88 88   88  88\n    8888  88  88   88  88888\n       88 88 888888888 88   88\n88888888  88 88     88 88    888888\n\n88  88  88   888    88888    888888\n88  88  88  88 88   88  88  88\n88 8888 88 88   88  88888    8888\n 888  888 888888888 88  88      88\n  88  88  88     88 88   88888888";
+}
