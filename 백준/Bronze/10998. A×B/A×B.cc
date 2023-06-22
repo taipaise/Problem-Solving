1fc336c0467fdcf50de1ce953c4de1a73bf582a9
taipaise <83569908+taipaise@users.noreply.github.com>
2023-06-22 21:59:50 +0900

    [Bronze V] Title: A×B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define rep(i, a, b) for(auto i = a; i < b; ++i)
+#define REP(i, a, b) for(auto i = a; i <= b; ++i)
+#define endl "\n"
+
+using namespace std;
+
+int main(void){
+    int a, b;
+    cin >> a >> b;
+    cout << a * b;
+}
