ff7bbc728578705ea7e31f10c08c358b552d452b
taipaise <83569908+taipaise@users.noreply.github.com>
2023-06-24 16:46:34 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define rep(i, a, b) for(auto i = a; i < b; ++i)
+#define REP(i, a, b) for(auto i = a; i <= b; ++i)
+#define endl "\n"
+
+using namespace std;
+
+int main(void){
+cout << "\\    /\\\n";
+cout << " )  ( ')\n";
+cout << "(  /  )\n";
+cout << " \\(__)|";
+}
