e70235c5b5dcbff63c8860d68210feda29d400a8
taipaise <83569908+taipaise@users.noreply.github.com>
2023-07-25 21:17:26 +0900

    [Bronze V] Title: N 찍기, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <queue>
+
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL);cout.tie(NULL)
+#define rep(i, a, b) for(auto i = a; i < b; ++i)
+#define REP(i, a, b) for(auto i = a; i <= b; ++i)
+typedef long long ll;
+using namespace std;
+
+int main(void){
+    int n;
+    cin >> n;
+    REP(i, 1, n) cout << i << "\n";
+}
