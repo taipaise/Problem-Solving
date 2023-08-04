48fc1bc87d73a4cf64e5503737300e107ebb3ded
taipaise <83569908+taipaise@users.noreply.github.com>
2023-08-04 22:13:08 +0900

    [Bronze V] Title: We love kriii, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define rep(i, a, b) for(auto i = a; i < b; ++i)
+#define REP(i, a, b) for(auto i = a; i <= b; ++i)
+#define endl "\n"
+
+using namespace std;
+
+int main(void){
+    cout <<"강한친구 대한육군\n";
+    cout <<"강한친구 대한육군\n";
+}
