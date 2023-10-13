9f0dc2dd9956f19c706c4fed597625238513782e
taipaise <83569908+taipaise@users.noreply.github.com>
2023-10-13 22:16:14 +0900

    [Silver II] Title: 최대 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <cstdlib>
+#include <algorithm>
+#include <set>
+#include <unordered_map>
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL);cout.tie(NULL)
+#define rep(i, a, b) for(auto i = a; i < b; ++i)
+using namespace std;
+
+priority_queue<int> pq;
+int n;
+
+int main(void){
+    FAST;
+    // freopen("input.txt", "r", stdin);
+    cin >> n;
+    int temp;
+    rep(i, 0, n){
+        cin >> temp;
+        if(temp){
+            pq.push(temp);
+        }
+        else{
+            if(pq.empty()) cout << 0 << "\n";
+            else{
+                cout << pq.top() << "\n";
+                pq.pop();
+            }
+        }
+    }
+}
