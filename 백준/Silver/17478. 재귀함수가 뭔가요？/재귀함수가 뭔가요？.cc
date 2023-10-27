3c368bee13f35a0c83ccb8a6f0104434402ebff7
taipaise <83569908+taipaise@users.noreply.github.com>
2023-10-27 21:15:56 +0900

    [Silver V] Title: 재귀함수가 뭔가요?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.cc
+++ 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.cc
@@ -13,32 +13,32 @@ using namespace std;
 
 int n;
 
-void print_underBar(){
-    cout << "____";
+void print_underBar(int cnt){
+    rep(i, 0, cnt) cout << "____";
 }
 
 void solve(int cnt){
     if(cnt == n){
-        rep(i, 0, cnt) print_underBar();
+        print_underBar(cnt);
         cout << "\"재귀함수가 뭔가요?\"\n";    
-        rep(i, 0, cnt) print_underBar();
+        print_underBar(cnt);
         cout << "\"재귀함수는 자기 자신을 호출하는 함수라네\"\n";
-        rep(i, 0, cnt) print_underBar();
+        print_underBar(cnt);
         cout << "라고 답변하였지.\n";
         return;
     }
-    rep(i, 0, cnt) print_underBar();
+    print_underBar(cnt);
     cout << "\"재귀함수가 뭔가요?\"\n";
-    rep(i, 0, cnt) print_underBar();
+    print_underBar(cnt);
     cout <<  "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n";
-    rep(i, 0, cnt) print_underBar();
+    print_underBar(cnt);
     cout <<  "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n";
-    rep(i, 0, cnt) print_underBar();
+    print_underBar(cnt);
     cout <<  "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"\n";
 
     solve(cnt + 1);
 
-    rep(i, 0, cnt) print_underBar();
+    print_underBar(cnt);
     cout << "라고 답변하였지.\n";
 }
 
