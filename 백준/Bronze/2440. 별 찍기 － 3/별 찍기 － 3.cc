e7ce4c934355fecf9c0744d2fd68e75eae60a225
taipaise <83569908+taipaise@users.noreply.github.com>
2023-11-11 14:42:32 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <queue>
+#include <cstring>
+
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define rep(i, a, b) for(auto i = a; i < b; ++i)
+#define REP(i, a, b) for(auto i = a; i <= b; ++i)
+#define endl "\n"
+
+using namespace std;
+int n;
+int main(void) {
+    cin >> n;
+    rep(i, 0, n){
+        rep(j, 0, n - i)
+            cout << "*";
+        cout << "\n";
+    }
+}
