d1bd00771ecc77da6409c4e3865c2f7b3a5d14a1
taipaise <83569908+taipaise@users.noreply.github.com>
2024-03-16 15:32:38 +0900

    [Silver III] Title: N과 M (2), Time: 12 ms, Memory: 79512 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.swift
@@ -0,0 +1,34 @@
+import Foundation
+
+let input = readLine()!.split(separator: " ").map { Int($0)! }
+let n = input.first!
+let m = input.last!
+
+var res: [Int] = []
+
+func solve(_ count: Int) {
+    
+    if count == m {
+        print(
+            res
+                .map { String($0) }
+                .joined(separator: " ")
+        )
+        return
+    }
+    
+    for i in 1...n {
+        if
+            let last = res.last,
+            last >= i
+        {
+            continue
+        }
+        
+        res.append(i)
+        solve(count + 1)
+        res.popLast()
+    }
+}
+
+solve(0)
