2524870ed88ca767a1d6baa477af8e9b96350e57
taipaise <83569908+taipaise@users.noreply.github.com>
2024-05-23 17:31:52 +0900

    [level 3] Title: 억억단을 외우자, Time: 6037.95 ms, Memory: 255 MB -BaekjoonHub

--- 프로그래머스/3/138475. 억억단을 외우자/억억단을 외우자.swift
+++ 프로그래머스/3/138475. 억억단을 외우자/억억단을 외우자.swift
@@ -1,26 +1,8 @@
 import Foundation
-var dic: [Int: Int] = [:]
-var offset = 1000000000
-var divisorCount = 0
-var checkedOffset = 1000000000
-
-func makeDic(from startNum: Int, to endNum: Int, _ divisors: [Int]) {
-      for i in stride(from: endNum, through: startNum, by: -1) {
-          guard divisorCount <= divisors[i] else { 
-              dic[i] = offset
-              continue
-          }
-        
-          divisorCount = divisors[i]
-          offset = i
-          dic[i] = offset
-      }
-}
 
 func solution(_ e:Int, _ starts:[Int]) -> [Int] {
-    offset = e
-    checkedOffset = e
     var res: [Int] = []
+    var dic: [Int: Int] = [:]
     var divisors = Array(repeating: 0, count: e + 1)
     
     let startNum = starts.min() ?? 1
@@ -31,13 +13,20 @@ func solution(_ e:Int, _ starts:[Int]) -> [Int] {
         }
     }
 
-    starts.forEach { 
-        if dic[$0] == nil {
-            makeDic(from: $0, to: checkedOffset, divisors)
-            checkedOffset = $0
+    var offset = e
+    var maxDivisorCount = 0
+    for i in stride(from: e, through: 1, by: -1) {
+        guard maxDivisorCount <= divisors[i] else { 
+            dic[i] = offset
+            continue
         }
-        res.append(dic[$0]!)
+        
+        maxDivisorCount = divisors[i]
+        offset = i
+        dic[i] = offset
     }
+    
+    starts.forEach { res.append(dic[$0]!) }
 
     return res
 }
