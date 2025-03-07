ea13a6f3deb9d7cfb44d8a9d8637340ad46b220e
taipaise <83569908+taipaise@users.noreply.github.com>
2025-03-07 13:54:53 +0900

    [Gold II] Title: 빵집, Time: 648 ms, Memory: 84996 KB -BaekjoonHub

---
+++ 백준/Gold/3109. 빵집/빵집.swift
@@ -0,0 +1,116 @@
+import Foundation
+
+// 아이디어: 가장 위쪽 칸부터 아래쪽 칸까지 순차적 탐색. 이동은 위쪽 대각선 -> 평향이동 -> 아래쪽 대각선
+
+enum Space: String {
+    case wall = "x"
+    case blank = "."
+    case pipeLine = "#"
+}
+
+enum Direction: Int, CaseIterable {
+    case up
+    case right
+    case down
+}
+
+struct Pos {
+    let y: Int
+    let x: Int
+
+    init(_ y: Int, _ x: Int) {
+        self.y = y
+        self.x = x
+    }
+}
+
+struct Queue<T> {
+    private var queue: [T] = []
+    private var head = 0
+    var isEmpty: Bool {
+        return head >= queue.count
+    }
+
+    mutating func push(_ element: T) {
+        queue.append(element)
+    }
+
+    mutating func pop() -> T? {
+        guard !isEmpty else { return nil }
+        defer { head += 1 }
+        return queue[head]
+    }
+
+}
+
+let numbers = readLine()!.split(separator: " ").map { Int(String($0))! }
+let height = numbers[0]
+let width = numbers[1]
+let dy = [-1, 0, 1]
+let dx = [1, 1, 1]
+let maxX = width - 1
+var boards: [[Space]] = []
+var visited: [[(isVisited: Bool, startY: Int)]] = Array(
+    repeating: Array(repeating: (false, 0), count: width),
+    count: height)
+
+func input() {
+    for _ in 0..<height {
+        let line = readLine()!.map { Space(rawValue: String($0))! }
+        boards.append(line)
+    }
+}
+
+func inRange(_ y: Int, _ x: Int) -> Bool {
+    return 0 <= y && y < height && 0 <= x && x < width
+}
+
+func isCross(_ destY: Int, _ destX: Int, _ direction: Direction) -> Bool {
+    switch direction {
+    case .up:
+        return boards[destY][destX - 1] == .pipeLine && boards[destY + 1][destX] == .pipeLine
+    case .down:
+        return boards[destY][destX - 1] == .pipeLine && boards[destY - 1][destX] == .pipeLine
+    default: // 오른쪽으로 이동한 경우 검사하지 않음 (아예 함수 실행 x)
+        return false
+    }
+}
+
+func move(_ y: Int, _ x: Int) -> Bool {
+    if x == maxX { return true }
+
+    for dir in Direction.allCases {
+        let ny = y + dy[dir.rawValue]
+        let nx = x + dx[dir.rawValue]
+
+        guard
+            inRange(ny, nx),
+            boards[ny][nx] == .blank
+        else { continue }
+
+        if
+            dir != .right,
+            isCross(ny, nx, dir)
+        { continue }
+
+        boards[ny][nx] = .pipeLine
+
+        if move(ny, nx) { return true }
+    }
+
+    return false
+}
+
+func solve() {
+    var res = 0
+
+    for y in 0..<height {
+        guard boards[y][0] == .blank else { continue }
+        if move(y, 0) { res += 1 }
+    }
+
+    print(res)
+}
+
+input()
+solve()
