5dfdaf26c8b6f968c430389e613f974aec92e1bb
taipaise <83569908+taipaise@users.noreply.github.com>
2025-03-18 18:15:53 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 148 ms, Memory: 83856 KB -BaekjoonHub

--- 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.swift
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.swift
@@ -2,88 +2,11 @@ import Foundation
 
 // 특정 칸에서 이동할 수 있는 최대 칸 수는 정해져 있다.
 // 대나무 양이 큰 칸부터, 이동할 수 있는 최대 칸 수를 구한다면? -> 힙 구조를 이용해 가장 큰 칸부터 시작한다
-// 재귀를 이용한 탑 다운 풀이.
-//   - memorization이 되어 있는 칸을 만나면 return.
-//    - 또는 움직일 수 없는 칸을 만나면 return
-
 enum Direction: Int, CaseIterable {
     case up
     case down
     case left
     case right
-}
-
-struct Pos {
-    let y: Int
-    let x: Int
-
-    init(_ y: Int, _ x: Int) {
-        self.y = y
-        self.x = x
-    }
-}
-
-struct Heap<T> {
-    private var heap: [T?] = [nil]
-    private var compare: (T, T) -> Bool
-
-    init(_ comp: @escaping (T, T) -> Bool) {
-        compare = comp
-    }
-
-    var isEmpty: Bool {
-        return heap.count <= 1
-    }
-
-    mutating func insert(_ element: T) {
-        heap.append(element)
-        heapifyUp()
-    }
-
-    mutating func pop() -> T? {
-        guard !isEmpty else { return nil }
-        heap.swapAt(1, heap.count - 1)
-        defer { heapifyDown() }
-        return heap.removeLast()
-    }
-
-    private mutating func heapifyUp() {
-        var index = heap.count - 1
-
-        while
-            index > 1,
-            let parent = heap[index / 2],
-            let child = heap[index],
-            compare(child, parent)
-        {
-            heap.swapAt(index, index / 2)
-            index /= 2
-        }
-    }
-
-    private mutating func heapifyDown() {
-        var index = 1
-
-        while index * 2 < heap.count {
-            var candidate = index * 2
-
-            if
-                candidate + 1 < heap.count,
-                let left = heap[candidate],
-                let right = heap[candidate + 1],
-                compare(right, left)
-            { candidate += 1 }
-
-            guard
-                let parent = heap[index],
-                let child = heap[candidate],
-                compare(child, parent)
-            else { return }
-
-            heap.swapAt(index, candidate)
-            index = candidate
-        }
-    }
 }
 
 let size = Int(readLine()!)!
@@ -97,43 +20,42 @@ var memorization: [[Int]] = Array(
     count: size)
 var res = 0
 
+for y in 0..<size {
+    let line = readLine()!.split(separator: " ").map { Int(String($0))! }
+    for x in 0..<size {
+        bamboo[y][x] = line[x]
+    }
+}
+
 func inRange(_ y: Int, _ x: Int) -> Bool {
     return 0 <= y && y < size && 0 <= x && x < size
 }
 
-func calculateMaxCount(_ pos: Pos) -> Int {
-    var res = 0
+func dfs(_ y: Int, _ x: Int) -> Int {
+    if memorization[y][x] > 0 { return memorization[y][x] }
+
+    memorization[y][x] = 1
 
     for dir in Direction.allCases {
-        let ny = pos.y + dy[dir.rawValue]
-        let nx = pos.x + dx[dir.rawValue]
+        let ny = y + dy[dir.rawValue]
+        let nx = x + dx[dir.rawValue]
 
         guard
             inRange(ny, nx),
-            bamboo[ny][nx] > bamboo[pos.y][pos.x]
+            bamboo[y][x] < bamboo[ny][nx]
         else { continue }
 
-        res = max(res, memorization[ny][nx])
+        memorization[y][x] = max(memorization[y][x], dfs(ny, nx) + 1)
     }
 
-    return res + 1
+    return memorization[y][x]
 }
 
 func solution() {
-    var heap = Heap<(pos: Pos, value: Int)>({ $0.value > $1.value })
-
     for y in 0..<size {
-        let line = readLine()!.split(separator: " ").map { Int(String($0))! }
         for x in 0..<size {
-            bamboo[y][x] = line[x]
-            heap.insert((pos: Pos(y, x), value: line[x]))
+            res = max(res, dfs(y, x))
         }
-    }
-
-
-    while let (pos, _) = heap.pop() {
-        memorization[pos.y][pos.x] = calculateMaxCount(pos)
-        res = max(res, memorization[pos.y][pos.x])
     }
 
     print(res)
