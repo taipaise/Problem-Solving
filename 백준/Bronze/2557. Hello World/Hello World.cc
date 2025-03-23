d7c293e66f6ddfb7d5fbd769d7e1b4a131560da8
taipaise <taipaise@gmail.com>
2025-03-23 16:53:04 +0900

    [Bronze V] Title: Hello World, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define endl "\n"
+
+using namespace std;
+
+int main() {
+    FAST;
+    cout << "Hello World!";
+    return 0;
+}
