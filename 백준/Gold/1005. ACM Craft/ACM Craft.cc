6582a3d465b088df759b3c78c6d108f2384897d1
taipaise <taipaise@gmail.com>
2025-03-24 13:40:35 +0900

    [Gold III] Title: ACM Craft, Time: 136 ms, Memory: 2844 KB -BaekjoonHub

--- 백준/Gold/1005. ACM Craft/ACM Craft.cc
+++ 백준/Gold/1005. ACM Craft/ACM Craft.cc
@@ -1,68 +1,89 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
-#include <cstring>
-#include <unordered_map>
-
-#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL);cout.tie(NULL)
-#define rep(i, a, b) for(int i = a; i < b; i++)
-#define REP(i, a, b) for(int i = a; i <= b; i++)
+#include <queue>
+#include <algorithm>
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define rep(i, a, b) for(auto i = a; i < b; ++i)
+#define REP(i, a, b) for(auto i = a; i <= b; ++i)
+#define endl "\n"
 using namespace std;
 
+// 데이터 구조
+struct Building {
+    int number;
+    int time;
+    int required = 0;
+    int requiredTime = 0;
+    vector<int> nexts;
+};
+
 int tc;
-int n; 
-int k;
-int build_time[1001];
-int complete_time[1001];
-vector<int> pre_building [1001];
 
-int target;
+vector<Building> makeBuildings(int buildingCount, int ruleCount) {
+    vector<Building> buildings(buildingCount + 1);
 
-void init(){
-    memset(build_time, -1, sizeof(build_time));
-    memset(complete_time, -1, sizeof(complete_time));
-    REP(i, 1, 1000) pre_building[i].clear();
+    REP(i, 1, buildingCount) {
+        buildings[i].number = i;
+        cin >> buildings[i].time;
+    }
+
+    rep(i, 0, ruleCount) {
+        int pre, next;
+        cin >> pre >> next;
+        ++buildings[next].required ;
+        buildings[pre].nexts.push_back(next);
+    }
+
+    return buildings;
 }
 
-int get_time(int x){
-    if(complete_time[x] != -1) return complete_time[x];
+int solve() {
+    int buildingCount, ruleCount;
+    int target;
+    cin >> buildingCount >> ruleCount;
+    vector<Building> buildings = makeBuildings(buildingCount, ruleCount);
+    cin >> target;
+    queue<Building> queue;
 
-    int res = 0;
-    rep(i, 0, pre_building[x].size()) res = max(res, get_time(pre_building[x][i]));
-        
-    res += build_time[x];
-    complete_time[x] = res;
+    // 시작 건물들을 큐에 넣어준다.
+    REP(i, 1, buildingCount) {
+        if (buildings[i].required == 0) {
+            queue.push(buildings[i]);
+        }
+    }
 
-    return complete_time[x];
-}
+    while (!queue.empty()) {
+        Building building = queue.front();
+        queue.pop();
 
-int main(void){
-    FAST;
-    // freopen("input.txt", "r", stdin);
-    cin >> tc;
-
-    REP(t, 1, tc){
-        cin >> n >> k;
-        init();
-
-        //해당 건물만 짓는 시간을 입력받음
-        REP(i, 1, n)
-            cin >> build_time[i];
-    
-        //선행 건물 정보를 입력 받음
-        int srt, end;
-        rep(i, 0, k){
-            cin >> srt >> end;
-            pre_building[end].push_back(srt);
+        if (building.number == target) {
+            return building.time + building.requiredTime;
         }
 
-        //선행 건물이 없는 건물들의 짓는 시간을 complete_time에 입력
-        REP(i, 1, n)
-            if(pre_building[i].empty()) complete_time[i] = build_time[i];
+        // 다음 빌딩 짓기까지 필요한 시간 갱신
+        rep(i, 0, building.nexts.size()) {
+            int next = building.nexts[i];
 
-        cin >> target;
-        cout << get_time(target) << "\n";
+            --buildings[next].required;
+            buildings[next].requiredTime = max(
+                buildings[next].requiredTime,
+                building.time + building.requiredTime);
+
+            if (buildings[next].required == 0) {
+                queue.push(buildings[next]);
+            }   
+        }
     }
+
+    return -1;
 }
 
+int main() {
+    FAST;
 
+    cin >> tc;
+    
+    rep(i, 0, tc) {
+        cout << solve() << endl;
+    }
+}
