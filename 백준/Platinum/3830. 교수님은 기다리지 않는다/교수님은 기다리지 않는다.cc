9c52007dd921047cac5ee0264702301b4e16d2ce
taipaise <taipaise@gmail.com>
2025-04-01 23:14:53 +0900

    [Platinum III] Title: 교수님은 기다리지 않는다, Time: 336 ms, Memory: 3712 KB -BaekjoonHub

--- 백준/Platinum/3830. 교수님은 기다리지 않는다/교수님은 기다리지 않는다.cc
+++ 백준/Platinum/3830. 교수님은 기다리지 않는다/교수님은 기다리지 않는다.cc
@@ -1,101 +1,93 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
-
-#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL);cout.tie(NULL)
-#define endl "\n"
-#define rep(i, a, b) for (auto i = a; i < b; ++i)
-#define REP(i, a, b) for (auto i = a; i <= b; ++i)
-#define pii pair<int, int>
-#define all(v) (v).begin(), (v).end()
-#define pb push_back
-#define INF numeric_limits<int>::max()
-#define PIV 1 << 20
+#include <string>
+#define rep(i, a, b) for(auto i = a; i < b; ++i)
+#define REP(i, a, b) for(auto i = a; i <= b; ++i)
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
 
 using namespace std;
+
 typedef long long ll;
-typedef unsigned long long ull;
 
-struct DisjointSet
-{
-    int n;
-    vector<int> parent;
-    vector<int> rank;
-    vector<ll> dist;
+struct DisjointSet {
+    vector<int> parents;
+    vector<int> ranks;
+    vector<ll> diff;
 
-    DisjointSet(int n) : n(n), parent(n + 1), rank(n + 1), dist(n + 1)
-    {
-        REP(i, 1, n)
-        {
-            parent[i] = i;
-            rank[i] = 1;
-            dist[i] = 0;
+    DisjointSet(int n) {
+        parents.resize(n + 1);
+        diff.resize(n + 1); // root로부터 무게 차이를 저장하는 벡터
+        ranks.resize(n + 1, 1);
+
+        REP(i, 0, n) {
+            parents[i] = i;
         }
     }
 
-    int find_parent(int u)
-    {
-        if (parent[u] == u) return u;
-        int root = find_parent(parent[u]);
-        dist[u] += dist[parent[u]];
-        return parent[u] = root;
+    void merge(int u, int v, int weight) {
+        int root_u = find(u);
+        int root_v = find(v);
+
+        if (root_u == root_v) { return; }
+
+        if (ranks[root_u] > ranks[root_v]) {
+            swap(root_u, root_v);
+            swap(u, v);
+            weight *= -1;
+        }
+
+        parents[root_u] = root_v;
+        diff[root_u] = diff[v] - diff[u] - weight;
+        
+
+        if (ranks[root_v] == ranks[root_u]) {
+            ++ranks[root_v];
+        }
     }
 
-    void merge_parent(int u, int v, int weight)
-    {
-        int root_u, root_v;
-        root_u = find_parent(u);
-        root_v = find_parent(v);
-
-        if (root_u == root_v) return;
-
-        if (rank[root_u] > rank[root_v])
-        {
-            dist[root_v] = dist[u] - dist[v] + weight;
-            parent[root_v] = root_u;
-        }
-        else
-        {
-            dist[root_u] = dist[v] - dist[u] - weight;
-            parent[root_u] = root_v;
+    int find(int u) {
+        if (parents[u] == u) {
+            return u;
         }
 
-        if (rank[root_u] == rank[root_v])
-            rank[root_v]++;
+        int root = find(parents[u]);
+        diff[u] += diff[parents[u]];
+
+        return parents[u] = root;
     }
 };
 
-char c;
-int n, m, a, b, weight;
+int n, m;
 
-int main(void)
-{
-    FAST;
-    //freopen("text.txt", "r", stdin);
-    while (1)
-    {
-        cin >> n >> m;
-        if (n == 0 && m == 0)
-            break;
+void solve() {
+    DisjointSet disjointSet = DisjointSet(n);
 
-        DisjointSet disjointSet = DisjointSet(n);
+    rep(i, 0, m) {
+        char type;
+        int a, b;
 
-        rep(i, 0, m)
-        {
-            cin >> c >> a >> b;
-            if (c == '!')
-            {
-                cin >> weight;
-                disjointSet.merge_parent(a, b, weight);
-            }
+        cin >> type >> a >> b;
+
+        if (type == '!') {
+            int weight;
+            cin >> weight;
+            disjointSet.merge(a, b, weight);
+        } else {
+            if (disjointSet.find(a) != disjointSet.find(b))
+                cout << "UNKNOWN\n";
             else
-            {
-                if (disjointSet.find_parent(a) != disjointSet.find_parent(b))
-                    cout << "UNKNOWN\n";
-                else
-                    cout << disjointSet.dist[b] - disjointSet.dist[a] << "\n";
-            }
+                cout << disjointSet.diff[b] - disjointSet.diff[a] << "\n";
         }
     }
-    return 0;
+}
+
+int main() {
+    FAST;
+
+    while (true) {
+        cin >> n >> m;
+
+        if (n == 0 && m == 0) break;
+        solve();
+    }
 }
