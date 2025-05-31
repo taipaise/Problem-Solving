5d1c2adac659fb4c8b57af1745a66285fb330036
taipaise <taipaise@gmail.com>
2025-05-31 14:52:58 +0900

    [level 2] Title: 하노이의 탑, Time: 4.21 ms, Memory: 32.1 MB -BaekjoonHub

--- 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.swift
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.swift
@@ -1,24 +1,24 @@
 import Foundation
-var result: [[Int]] = []
 
-func move(_ n: Int, _ start: Int, _ sub: Int, _ end: Int) {
-    if n == 1 {
-        // 가장 아래에 있는 원판(n)을 도착점으로 이동
-        result.append([start, end])
+var res: [[Int]] = []
+
+func solution(_ n:Int) -> [[Int]] {
+    hanoi(n, 1, 2, 3)
+    return res
+}
+
+func hanoi (
+    _ num: Int,
+    _ from: Int,
+    _ by: Int,
+    _ dest: Int
+) {
+    if num == 1 {
+        res.append([from, dest])
         return
     }
     
-    // 1. 아래에서 두번째 ~ 맨 위의 원반을 시작점에서 도착점을 경유하여 경유지로 옮김
-    move(n - 1, start, end, sub)
-    
-    // 가장 아래에 있는 원판(n)을 도착지로 이동시킨다.
-    result.append([start, end])
-    
-    // 1번에서 옮긴 나머지 원판들을 end로 옮김
-    move(n - 1, sub, start, end)
-}
-
-func solution(_ n:Int) -> [[Int]] {
-    move(n, 1, 2, 3)
-    return result
+    hanoi(num - 1, from, dest, by)
+    res.append([from, dest])
+    hanoi(num - 1, by, from, dest)
 }
