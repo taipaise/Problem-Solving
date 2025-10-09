b9447a05ee822a56b8e9835e2992a18a37618573
taipaise <taipaise@gmail.com>
2025-10-09 11:44:10 +0900

    [Platinum V] Title: 가장 긴 증가하는 부분 수열 5, Time: 600 ms, Memory: 155132 KB -BaekjoonHub

--- 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.swift
+++ 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.swift
@@ -1,57 +1,58 @@
 import Foundation
 
-func findLisIndex(_ lis: [Int], _ number: Int) -> Int {
+
+func findIndex(_ n: Int, _ lis: [Int]) -> Int {
     var lo = -1
     var hi = lis.count
 
     while lo + 1 < hi {
         let mid = (lo + hi) >> 1
 
-        if lis[mid] < number {
+        if lis[mid] < n {
             lo = mid
         } else {
             hi = mid
         }
     }
-
     return hi
 }
 
 func solution() {
     let n = Int(readLine()!)!
-    let numbers = readLine()!.split(separator: " ").map { Int(String($0))! }
+    let numbers = readLine()!.split(separator: " ").map { Int($0)! }
+
     var lis: [Int] = []
-    var lisIndexRecord = Array(repeating: 0, count: n)
+    var indices: [Int] = Array(repeating: -1, count: n)
 
-    for (index, number) in numbers.enumerated() {
-        let lisIndex = findLisIndex(lis, number)
-        lisIndexRecord[index] = lisIndex
+    numbers.enumerated().forEach { (index, num) in
+        let lisIndex = findIndex(num, lis)
 
-        if lis.count == lisIndex {
-            lis.append(number)
+        indices[index] = lisIndex
+
+        if lisIndex == lis.count {
+            lis.append(num)
         } else {
-            lis[lisIndex] = number
+            lis[lisIndex] = num
         }
     }
 
-    // lis 길이
     print(lis.count)
-
-
-    // lis 역추적하기
-    var result: [Int] = []
-    var curLisIndex = lisIndexRecord.max()!
+    var answer: [Int] = []
+    var curLisIndex = lis.count - 1
 
     for i in stride(from: n - 1, through: 0, by: -1) {
         guard curLisIndex >= 0 else { break }
 
-        if curLisIndex == lisIndexRecord[i] {
-            result.append(numbers[i])
-            curLisIndex -= 1
-        }
+        let number = numbers[i]
+        let lisIndex = indices[i]
+
+        guard lisIndex == curLisIndex else { continue }
+
+        answer.append(number)
+        curLisIndex -= 1
     }
 
-    print(result.reversed().map { "\($0)" }.joined(separator: " "))
+    print(answer.reversed().map { String($0) }.joined(separator: " "))
 }
 
 solution()
